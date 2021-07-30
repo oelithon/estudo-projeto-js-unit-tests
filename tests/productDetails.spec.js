496a65a17c8bca72c14574bc73150846bcaa18ee
Elithon Silva <elithonsilva@gmail.com>
2021-07-30 09:36:21 -0300

    Requisito 06

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,9 +33,13 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // Teste que os dois productIds terminam com 123.
   });
 });
+
+// O método Array.isArray() retorna true se um objeto é uma array, e false se não é.
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
